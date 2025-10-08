b2db2f160eb1e04b370883dff95f02490f9aa8b6
777Arman <arman.bgdr@gmail.com>
2025-10-08 15:07:13 +0200

    Set up projet

---
+++ src/main/java/com/projet_ddc/projet_ddc/ProjetDdcApplication.java
@@ -0,0 +1,13 @@
+package com.projet_ddc.projet_ddc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetDdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetDdcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projet_ddc/projet_ddc/ProjetDdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet_ddc.projet_ddc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetDdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
