a64ad4f5526719114dc307835fb31a696314fd77
777Arman <arman.bgdr@gmail.com>
2025-10-18 10:24:19 +0200

    css et js modifié

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,3 +1,15 @@
 document.getElementById("b_envoie").addEventListener("click", function() {
-    alert("Vos fichiers ont bien été envoyés !");
+    const verif_cv = document.getElementById("CV");
+    const verif_lm = document.getElementById("motiv");
+    if(verif_cv.files.length === 0){
+        alert("Veuillez rajouter un CV")
+    }
+    else {
+        if(verif_lm.files.length === 0){
+            alert("Veuillez rajouter une lettre de motivation")
+        }
+        else{
+            alert("Vos fichiers ont bien été envoyés !");
+        }
+    }
 });
